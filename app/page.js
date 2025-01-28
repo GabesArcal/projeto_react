94d2909cdb3020aa735e6dd86c075dbb72e1bdf7
Gabriel Dias <gabesarcal215@gmail.com>
2025-01-27 21:47:48 -0300

    Criando documentação

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main>
       <h1>Enviando para o Github</h1>
-      <p>Ativiade Realizada</p>
+      <p>Ativiade em andamento...</p>
+      <p>Teste nova versão</p>
 
     </main>
   );
